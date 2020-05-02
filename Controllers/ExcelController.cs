0a8c57bca8de2a6c6fd20d602c94291f3250f4f7
enes sezgin <enes.sezgin90@hotmail.com>
2020-05-02 19:10:24 +0300

    EPPlus NuGet Package eklendi

---
+++ Controllers/ExcelController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ECommerceCMS.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("EnableCors")]
+    [Authorize]
+    public class ExcelController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("[action]")]
+        public FileResult DownloadExcel(int type)
+        {
+            using (ExcelPackage excel = new ExcelPackage())
+            {
+                excel.Workbook.Worksheets.Add("Markalar");
+
+                var headerRow = new List<string[]>() { new string[] { "Marka Adı", "Web Sitesi Url'i" } };
+
+                // Determine the header range (e.g. A1:D1)
+                string headerRange = "A1:" + Char.ConvertFromUtf32(headerRow[0].Length + 64) + "1";
+
+                // Target a worksheet
+                var worksheet = excel.Workbook.Worksheets["Worksheet1"];
+
+                // Popular header row data
+                worksheet.Cells[headerRange].LoadFromArrays(headerRow);
+
+                FileInfo excelFile = new FileInfo(@"C:\Users\e_sez\OneDrive\Desktop\markalar.xlsx");
+                excel.SaveAs(excelFile);
+
+                using (var stream = new MemoryStream(excel.GetAsByteArray()))
+                {
+                    return File(stream.ToArray(), "XLSX", "Model Şablonu");
+                }
+            }
+        }
+    }
+}
